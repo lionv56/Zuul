8b9bb1fb0e1dd2b30d99b353807940606b586b70
Bankai <lionv466@gmail.com>
2026-01-27 17:26:18 +0100

    Update Inventory.cs

--- src/Inventory.cs
+++ src/Inventory.cs
@@ -12,8 +12,8 @@ class Inventory
     // methods 
     public bool Put(string itemName, Item item)
     {
-        // TODO implementeer: 
-        // Check het gewicht van het Item 
+        // TODO implementeer:
+        // Check het gewicht van het Item
         // Is er genoeg ruimte in de Inventory? 
         // Past het Item? 
         // Zet Item in de Dictionary 
@@ -27,5 +27,46 @@ class Inventory
         // Verwijder Item uit Dictionary (als gevonden)
         // Return Item of null 
         return null;
+    }
+    private int CurrentWeight()
+    {
+        // TODO implementeer: 
+        // Bereken het totale gewicht van alle Items in de Inventory
+        int totalWeight = 0;
+        items.Values.ToList().ForEach(item => totalWeight += item.Weight);
+        return totalWeight;
+    }
+    private bool CanFit(Item item)
+    {
+        // TODO implementeer: 
+        // Check of het Item past in de Inventory
+        if (item == null) return false;
+        if (item.Weight <= maxWeight) return true;
+        else
+        {
+            return false;
+        }
+    }
+    private bool HasSpaceFor(Item item)
+    {
+        // TODO implementeer: 
+        // Check of er genoeg ruimte is voor het Item 
+        if (item == null) return false;
+        if (item.Weight + CurrentWeight() <= maxWeight) return true;
+        else
+        {
+            return false;
+        }
+    }
+    private bool ItemExists(string itemName)
+    {
+        // TODO implementeer: 
+        // Check of het Item al in de Inventory zit
+        itemName
+        if (items.ContainsKey(itemName)) return true;
+        else
+        {
+            return false;
+        }
     }
 } 
